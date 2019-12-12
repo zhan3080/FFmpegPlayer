40d15096b5cb5563fb108f86e8f76f6a723bcf78
hzz <hzz@hpplay.cn>
2019-12-12 11:59:52 +0800

    multi_so:modify the build script
    修改编译脚本

--- base/multi_so/build.sh
+++ base/multi_so/build.sh
@@ -5,4 +5,8 @@ ndk-build NDK_APPLICATION_MK=Application.mk NDK_PROJECT_PATH=. clean
 
 ndk-build NDK_APPLICATION_MK=Application.mk NDK_PROJECT_PATH=.
 
+rm -rf ../../ffmpeglibrary/libs/armeabi-v7a/*
+
+cp -rf libs/armeabi-v7a/*.so ../../ffmpeglibrary/libs/armeabi-v7a/
+
 echo "build end  **************************"
